d8c6da860b6ebfb2fac500ca8c6739e51f8dbe9c
lokchonmou <lok.chon.mou@gmail.com>
2018-04-26 19:44:33 +0800

    version 1.2
    debug

--- TimeRTCSet/TimeRTCSet.ino
+++ TimeRTCSet/TimeRTCSet.ino
@@ -5,7 +5,7 @@
 #include <DS3231.h>
 #include <SoftwareSerial.h>
 
-#define TIME_HEADER  "T"  
+#define TIME_HEADER  "T"
 #define LED_PIN     9
 #define NUM_LEDS    60
 #define CHIPSET     WS2811
@@ -36,13 +36,13 @@ void loop()  {
       int h = mySerial.parseInt();
       int m = mySerial.parseInt();
       int s = mySerial.parseInt();
-      
+
       mySerial.print(h);
       mySerial.print(',');
       mySerial.print(m);
       mySerial.print(',');
       mySerial.println(s);
-      rtc.setTime(h,m,s);
+      rtc.setTime(h, m, s);
     }
     mySerial.println("OK");
   }
@@ -62,21 +62,21 @@ void loop()  {
 }
 
 void to_analog_clock() {
-  int _hour = t.hour>=12? t.hour-12: t.hour;
+  int _hour = t.hour >= 12 ? t.hour - 12 : t.hour;
 
   for (byte i = 0; i <= 59; i++) leds[i] = CRGB::Black;
   for (byte i = 0; i <= 59; i += 5) leds[i] = CRGB::White;
+
   leds[_hour * 5 + t.min / 12] = CRGB::Red;
-
   leds[t.min] = CRGB::Green;
-  if (_hour * 5 + t.min / 12 == t.min) leds[t.min] = CRGB::Yellow;
-
   leds[t.sec] = CRGB::Blue;
+
+  if (_hour * 5 + t.min / 12 == t.min) leds[t.min] = CRGB(255, 255, 0);
   if (_hour * 5 + t.min / 12 == t.sec) leds[t.sec] = CRGB(255, 0, 255);
   if (t.min == t.sec) leds[t.sec] = CRGB(0, 255, 255);
   if (_hour * 5 + t.min / 12 == t.min && t.min == t.sec) leds[t.sec] = CRGB(255, 255, 255);
 
-  if (t.sec == 0 && t.min%5 == 0) show_rainbow = HIGH;
+  if (t.sec == 0 && t.min % 5 == 0) show_rainbow = HIGH;
   else show_rainbow = LOW;
 
   FastLED.show();
